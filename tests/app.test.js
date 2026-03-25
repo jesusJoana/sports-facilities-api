2a416d306bf9dc85a3d063eeb9aefada0fbbc24d
Jesus Joana <jesusjoanaazuara@gmail.com>
2026-03-25 12:14:40 +0100

    Configuración de jest y primer test

--- tests/app.test.js
+++ tests/app.test.js
@@ -1,7 +1,13 @@
 const request = require('supertest');
 const app = require('../src/app');
 
-test('GET /', async () => {
-    const res = await request(app).get('/');
-    expect(res.statusCode).toBe(200);
+describe('GET /', () => {
+    test('should return API running message', async () => {
+        const res = await request(app).get('/');
+
+        expect(res.statusCode).toBe(200);
+        expect(res.body).toEqual({
+            message: 'Sports Facilities API is running'
+        });
+    });
 });
