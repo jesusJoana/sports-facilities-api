debcc375b115f6bc2c3f608a6730ff89ab373dca
Jesus Joana <jesusjoanaazuara@gmail.com>
2026-03-25 12:56:10 +0100

    Preparación del esqueleto de rutas

--- src/app.js
+++ src/app.js
@@ -1,5 +1,9 @@
 const express = require('express');
 
+const installationsRoutes = require('./routes/installations.routes');
+const sportsRoutes = require('./routes/sports.routes');
+const weatherRoutes = require('./routes/weather.routes');
+
 const app = express();
 
 app.use(express.json());
@@ -8,4 +12,9 @@ app.get('/', (req, res) => {
     res.json({ message: 'Sports Facilities API is running' });
 });
 
+app.use('/installations', installationsRoutes);
+app.use('/sports', sportsRoutes);
+app.use('/weather-records', weatherRoutes);
+
 module.exports = app;
+
